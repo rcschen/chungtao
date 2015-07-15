2da1960dcb23ff71fc254228b2c58c71709e09ef
unknown <cschen@110318-NB-CSSI.iii.org.tw>
2015-07-15 14:06:09 +0800

    [arduino/yun] rest api environment setting

---
+++ arduino/receiver_yun_sketch/arduino_robot.py
@@ -0,0 +1,82 @@
+import base64
+import httplib
+import urllib
+from  msvcrt import *
+
+class robotConnect(object):
+      def __init__( self, robotAddress = 'arduino.cschen' ):
+          self.robotAddress = robotAddress
+          self.connection = httplib.HTTPConnection(self.robotAddress)
+
+      def stop(self):
+          cmd = "/arduino/robot/stop"            
+          self._connect(cmd)
+
+      def left(self):
+          cmd = "/arduino/robot/turnleft"            
+          self._connect(cmd)
+
+      def right(self):
+          cmd = "/arduino/robot/turnright"            
+          self._connect(cmd)
+
+      def fullfw(self):
+          cmd = "/arduino/robot/fullfw"            
+          self._connect(cmd)
+		  
+      def back(self):
+          cmd = "/arduino/robot/back"            
+          self._connect(cmd)
+
+      def test(self):
+          cmd = "/arduino/robot/test/123"
+          self._connect(cmd)
+
+      def _connect(self, action):
+          print "action is ", action  
+          self.connection.putrequest("GET", action)
+          self.connection.endheaders()
+          try:
+             response = self.connection.getresponse()
+             return response
+          except Exception as e:
+             print e
+             
+
+if __name__ == '__main__':
+   address = raw_input('input address:')
+   if len(address) == 0:
+      address = '192.168.1.235'
+   robotConnect = robotConnect(address)
+   print "The server address is: ", address
+   leave = True
+   print dir(robotConnect)
+   while leave :
+      try:
+         control = getch() 
+         if control == "s":
+            print "stop"
+            robotConnect.stop()
+         elif control == "d":
+            print "right"
+            robotConnect.right()
+         elif control == "a":
+            print "left"
+            robotConnect.left()
+         elif control == "w":
+            print "fullfw"
+            robotConnect.fullfw()
+         elif control == "x":
+            print "back"
+            robotConnect.back()
+         elif control == "t":
+            print "test"
+            robotConnect.test()			
+         elif control =="q":
+            print "bye-bye"
+            leave = False
+         else:
+            print "Not control command"
+      except Exception as e:
+         print e
+
--- arduino/receiver_yun_sketch/receiver_yun_sketch.ino
+++ arduino/receiver_yun_sketch/receiver_yun_sketch.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
 
+YunServer restServer;
+
+void setup() {
+     Wire.begin();
+     Bridge.begin();
+     Serial.begin(9600);
+     restServer.listenOnLocalhost();
+     restServer.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+     YunClient restClient = restServer.accept();
+     if (restClient) {
+          process(restClient);
+          restClient.stop();
+     }
+     delay(50); 
+     Serial.println("----------");
 }
+
+void process(YunClient restClient) {
+  
+     String command = restClient.readStringUntil('/');
+     Serial.println(">>>>>>>>>>"+command);
+     if (command == "robot") {   
+        //robotCommand(client);
+     }
+}
+
+
