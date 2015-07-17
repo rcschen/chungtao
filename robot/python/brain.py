07686043821ef47af01b1d1114ced9fc3aa04a60
rcschen <chungshiuan@gmail.com>
2015-07-17 15:23:29 +0800

    [robot/python] use queue to bridge brain and eyes

---
+++ robot/python/brain.py
@@ -0,0 +1,17 @@
+import threading
+
+class Brain(threading.Thread):
+      def __init__(self, queue):
+          super(Brain,  self ).__init__(name = "Brain")
+          self.frameQueue = queue
+
+      def run(self):
+          while True:
+             frame = self._getFrame()
+             if frame:
+                frame.showFrame()
+
+      def _getFrame(self):
+          if not self.frameQueue.isEmpty():
+             return self.frameQueue.get()
+
--- robot/python/robot.py
+++ robot/python/robot.py
@@ -1,7 +1,20 @@
+from eyes import Eyes
+from brain import Brain
 
-class Robot:
-      def start(self):
-          pass
+from queue import QueueFactory
+
+class Robot(object):
+      def __init__(self):
+          self._senses = []
+          self._set_senses()
+          self._start_senses()
+
+      def _set_senses(self):
+          print "No senses are set!!!"
+       
+      def _start_senses(self):
+          for sense in self._senses:
+              sense.start()              
 
       def controller(self):
           pass
@@ -13,6 +26,17 @@ class Robot:
           pass
 
 class Chuntao(Robot):
-      def __init__(self):
+      def __init__(self, eyesUrl):
+          self._eyesUrl = eyesUrl 
+          self.queue = QueueFactory().getQueue()
+          print dir(self.queue)
           super(Chuntao, self).__init__()
 
+      def _set_senses(self):
+          self._senses.append(Eyes(self._eyesUrl, self.queue))
+          self._senses.append(Brain(self.queue))
+
+
+
+if __name__ == '__main__':
+   rob = Chuntao('http://192.168.1.235:8080/?action=stream')          
