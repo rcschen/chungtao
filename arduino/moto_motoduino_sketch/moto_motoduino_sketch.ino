bc9709fbc328589127dc274847b96b0d6997f862
unknown <cschen@110318-NB-CSSI.iii.org.tw>
2015-07-31 18:12:22 +0800

    [arduino/motoduino] robot arduino complete

---
+++ arduino/moto_motoduino_sketch/moto_motoduino_sketch.ino
@@ -0,0 +1,58 @@
+
+#include <Wire.h>
+
+int speed_motor1 = 6;  
+int speed_motor2 = 5;
+int direction_motor1 = 8;
+int direction_motor2 = 7;
+
+void setup()
+{
+
+  for(int i=4;i<=7;i++)
+  {  
+    pinMode(i, OUTPUT);  
+  }
+  Wire.begin(4);                
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);           
+}
+
+void loop()
+{
+  delay(100);
+  Serial.println(">>>>>>>");
+}
+
+
+void receiveEvent(int howMany)
+{
+    int pwm1 = Wire.read();
+    Serial.print(pwm1); 
+    char c = Wire.read();
+    Serial.print(c);
+  
+    int dir1 = Wire.read();
+    Serial.print(dir1); 
+    c = Wire.read();
+    Serial.print(c);
+  
+    int pwm2 = Wire.read();
+    Serial.print(pwm2); 
+    c = Wire.read();
+    Serial.print(c);
+  
+    int dir2 = Wire.read();
+    Serial.println(dir2);
+   
+  // Apply these commands to the robot motors
+    send_motor_command(speed_motor1,direction_motor1,pwm1,dir1);
+    send_motor_command(speed_motor2,direction_motor2,pwm2,dir2);
+}
+
+// Function to command a given motor of the robot
+void send_motor_command(int speed_pin, int direction_pin, int pwm, boolean dir)
+{
+    analogWrite(speed_pin,pwm); // Set PWM control, 0 for stop, and 255 for maximum speed
+    digitalWrite(direction_pin,dir);
+}
--- arduino/receiver_yun_sketch/receiver_yun_sketch.ino
+++ arduino/receiver_yun_sketch/receiver_yun_sketch.ino
@@ -2,8 +2,13 @@
 #include <Bridge.h>
 #include <YunServer.h>
 #include <YunClient.h>
+//#include <iostream>
+//#include <vector>
 
+//using namespace std;
 YunServer restServer;
+int wireAddress = 4;
+const int fullFactorValue = 100;
 
 void setup() {
      Wire.begin();
@@ -20,13 +25,15 @@ void loop() {
           restClient.stop();
      }
      delay(50); 
+     //Serial.println("----");
 }
 
 void process(YunClient restClient) {
   
-     String command = restClient.readStringUntil('\');
+     String command = restClient.readStringUntil('/');
      Serial.println(">>>>>>>>>>"+command);
-     if (command == "fullfw") {   
+     if (command == "fullfw") {  
+      Serial.println("fffffff"); 
          fullfw(restClient);
      }
      else if (command == "back") {
@@ -46,18 +53,81 @@ void process(YunClient restClient) {
      }
 }
 
+void sendControl(int control[]) {
+    Wire.beginTransmission(wireAddress);
+    Serial.print(control[0]);
+    Serial.print(control[1]);
+    Serial.print(control[2]);
+    Serial.println(control[3]);
+    Serial.println("-------------");
+    Wire.write(control[0]);
+    Wire.write(",");
+    Wire.write(control[1]);
+    Wire.write(",");
+    Wire.write(control[2]);
+    Wire.write(",");
+    Wire.write(control[3]);
+   Wire.endTransmission();
+}
+
 void fullfw(YunClient client) {
+    String para = client.readStringUntil('\n');
+    float factor = para.toFloat();
+    int control[4];
+    control[0] = 1;
+    control[1] = fullFactorValue*factor ;
+    control[2] = 0;
+    control[3] = fullFactorValue*factor;
+    sendControl(control);
 }
 
 void back(YunClient client) {
+    String para = client.readStringUntil('\n');
+    float factor = para.toFloat();
+    int control[4];
+    control[0] = 0;
+    control[1] = fullFactorValue*factor ;
+    control[2] = 1;
+    control[3] = fullFactorValue*factor;
+    sendControl(control);
 }
 
 void turnRight(YunClient client){
+    String para = client.readStringUntil('\n');
+    float factor = para.toFloat();
+    int control[4];
+    control[0] = 1;
+    control[1] = fullFactorValue*factor ;
+    control[2] = 0;
+    control[3] = fullFactorValue;
+    sendControl(control);
 }
 
 void turnLeft(YunClient client) {
+    String para = client.readStringUntil('\n');
+    float factor = para.toFloat();
+    int control[4];
+    control[0] = 1;
+    control[1] = fullFactorValue;
+    control[2] = 0;
+    control[3] = fullFactorValue*factor;
+    sendControl(control);
 }
 
 void getStop (YunClient client) {
+     String para = client.readStringUntil('\n');
+    float factor = para.toFloat();
+    int control[4];
+    control[0] = 0;
+    control[1] = 0;
+    control[2] = 0;
+    control[3] = 0;
+    sendControl(control);
 }
+
+
+
+
+
+
 
